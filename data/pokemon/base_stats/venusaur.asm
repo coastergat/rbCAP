121586382754f874d82627cb8430eb41f8d077c1
Coop <117552034+coastergat@users.noreply.github.com>
2025-02-28 16:39:08 -0800

    Update venusaur.asm

--- data/pokemon/base_stats/venusaur.asm
+++ data/pokemon/base_stats/venusaur.asm
@@ -1,9 +1,9 @@
 	db DEX_VENUSAUR ; pokedex id
 
-	db  80,  82,  83,  80, 100
+	db  70,  87,  87,  81, 100
 	;   hp  atk  def  spd  spc
 
-	db GRASS, POISON ; type
+	db GRASS, WATER ; type
 	db 45 ; catch rate
 	db 208 ; base exp
 
@@ -15,9 +15,10 @@
 
 	; tm/hm learnset
 	tmhm SWORDS_DANCE, TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  \
-	     HYPER_BEAM,   RAGE,         MEGA_DRAIN,   SOLARBEAM,    MIMIC,        \
-	     DOUBLE_TEAM,  REFLECT,      BIDE,         REST,         SUBSTITUTE,   \
-	     CUT
+	     BUBBLEBEAM,   WATER_GUN,    HYPER_BEAM,   RAGE,         MEGA_DRAIN,   \
+	     SOLARBEAM,    MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         \
+	     SKULL_BASH,   REST,         SUBSTITUTE,   CUT,          SURF          \
+             STRENGTH
 	; end
 
 	db 0 ; padding
