0918e7b5c74612e362a513dbabb920bf35cbacce
Coop <117552034+coastergat@users.noreply.github.com>
2025-02-28 16:59:50 -0800

    Update arbok.asm

--- data/pokemon/base_stats/arbok.asm
+++ data/pokemon/base_stats/arbok.asm
@@ -1,9 +1,9 @@
 	db DEX_ARBOK ; pokedex id
 
-	db  60,  85,  69,  80,  65
+	db  68,  50, 120,  35,  86
 	;   hp  atk  def  spd  spc
 
-	db POISON, POISON ; type
+	db NORMAL, NORMAL ; type
 	db 90 ; catch rate
 	db 147 ; base exp
 
@@ -15,9 +15,11 @@
 
 	; tm/hm learnset
 	tmhm TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  HYPER_BEAM,   \
-	     RAGE,         MEGA_DRAIN,   EARTHQUAKE,   FISSURE,      DIG,          \
-	     MIMIC,        DOUBLE_TEAM,  BIDE,         SKULL_BASH,   REST,         \
-	     ROCK_SLIDE,   SUBSTITUTE,   STRENGTH
+	     SUBMISSION,   COUNTER,      RAGE,         MEGA_DRAIN,   DRAGON_RAGE,  \
+	     EARTHQUAKE,   FISSURE,      DIG,          PSYCHIC_M,    TELEPORT,     \
+             MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         METRONOME,    \
+             SELFDESTRUCT, SWIFT,        DREAM_EATER,  REST,         PSYWAVE,      \
+	     ROCK_SLIDE,   TRI_ATTACK,   SUBSTITUTE,
 	; end
 
 	db 0 ; padding
