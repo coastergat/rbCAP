6a8d987ebbf69d22443be9cb19bc32d8df016888
Coop <117552034+coastergat@users.noreply.github.com>
2025-02-28 16:46:58 -0800

    Update butterfree.asm

--- data/pokemon/base_stats/butterfree.asm
+++ data/pokemon/base_stats/butterfree.asm
@@ -1,9 +1,9 @@
 	db DEX_BUTTERFREE ; pokedex id
 
-	db  60,  45,  50,  70,  80
+	db  45,  75,  45,  55,  85
 	;   hp  atk  def  spd  spc
 
-	db BUG, FLYING ; type
+	db BUG, BUG ; type
 	db 45 ; catch rate
 	db 160 ; base exp
 
@@ -14,10 +14,11 @@
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
-	tmhm RAZOR_WIND,   WHIRLWIND,    TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,  \
-	     HYPER_BEAM,   RAGE,         MEGA_DRAIN,   SOLARBEAM,    PSYCHIC_M,    \
-	     TELEPORT,     MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         \
-	     SWIFT,        REST,         PSYWAVE,      SUBSTITUTE
+	tmhm RAZOR_WIND,   SWORDS_DANCE, WHIRLWIND,    TOXIC,        TAKE_DOWN,    \
+	     DOUBLE_EDGE,  HYPER_BEAM,   RAGE,         MEGA_DRAIN,   PSYCHIC_M,    \
+	     TELEPORT,     MIMIC,        DOUBLE_TEAM,  BIDE,         METRONOME,    \
+	     SWIFT,        REST,         PSYWAVE,      SUBSTITUTE,   CUT,          \
+             FLY
 	; end
 
 	db 0 ; padding
